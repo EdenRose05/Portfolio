f8d0c93d73ab65a3daad40d695c9f9e6a1366be4
Anaïs Schutz <anais.schutz@uha.fr>
2023-10-13 08:27:09 +0200

    a
    a

--- JS/scriptAccueil.js
+++ JS/scriptAccueil.js
@@ -1,6 +1,32 @@
-document.getElementById("element1").addEventListener("click", function contact(){
-
-const div= document.querySelector(".contact");
-
-div.style.top ='0';
+document.getElementById('button').addEventListener('click', function () {
+    navigate('contact', 'accueil', 'block');
 });
+
+document.getElementById('svg').addEventListener('click', function () {
+    navigate('accueil', 'contact', 'block');
+});
+document.getElementById('element1').addEventListener('click', function () {
+    navigate('accueil', 'contact', 'block');
+});
+
+document.getElementById('element2').addEventListener('click', function () {
+    navigate('accueil', 'grid', 'grid');
+});
+
+document.getElementById('link4').addEventListener('click', function () {
+    navigate('grid', 'accueil', 'block');
+});
+
+function navigate(fromId, toId, displayValue) {
+    const fromViewport = document.getElementById(fromId);
+    const toViewport = document.getElementById(toId);
+
+    fromViewport.style.transform = 'translateY(' + (fromId === 'accueil' ? '-100%' : '100%') + ')';
+    toViewport.style.transform = 'translateY(0)';
+
+    setTimeout(function () {
+        fromViewport.style.display = 'none';
+        toViewport.style.display = displayValue;
+        toViewport.style.transform = 'translateY(0)';
+    }, 500); // Adjust this time to match the transition time (in milliseconds)
+}
