dd3e5608cc2eb2afbd0ffa5a4c0b7af860d8a16b
Anaïs Schutz <anais.schutz@uha.fr>
2023-10-13 11:39:24 +0200

    a
    a

--- JS/scriptAccueil.js
+++ JS/scriptAccueil.js
@@ -1,6 +1,12 @@
 document.getElementById('button').addEventListener('click', function () {
     navigate('contact', 'accueil', 'block');
 });
+document.getElementById('logo').addEventListener('click', function () {
+    navigate('contact', 'accueil', 'block');
+});
+document.getElementById('logo1').addEventListener('click', function () {
+    navigate('grid', 'accueil', 'block');
+});
 
 document.getElementById('svg').addEventListener('click', function () {
     navigate('accueil', 'contact', 'block');
@@ -28,5 +34,49 @@ function navigate(fromId, toId, displayValue) {
         fromViewport.style.display = 'none';
         toViewport.style.display = displayValue;
         toViewport.style.transform = 'translateY(0)';
-    }, 500); // Adjust this time to match the transition time (in milliseconds)
+    }, 500);
 }
+
+function open(contentId){
+    document.getElementById('modal').style.display = 'block';
+    switch (contentId){
+        case 'aPropos':
+            document.getElementById('contentContainer').innerHTML = document.getElementById('propos').innerHTML;
+            break;
+        case 'mesProjets':
+            document.getElementById('contentContainer').innerHTML = document.getElementById('projets').innerHTML;
+            break;
+        case 'ceQueJeSaisFaire':
+            document.getElementById('contentContainer').innerHTML = document.getElementById('compets').innerHTML;
+            break;
+        default:
+            document.getElementById('contentContainer').innerHTML = 'Default Content';
+        
+    }
+}
+
+function close(){
+    document.getElementById('modal').style.display = 'none';
+}
+
+window.addEventListener('click', function(event){
+    const modal= this.document.getElementById('modal');
+    if(event.target == modal){
+        close();
+    }
+});
+
+document.getElementById('proposLink').addEventListener('click', function(event){
+    event.preventDefault();
+    open('aPropos');
+});
+document.getElementById('projetsLink').addEventListener('click', function(event){
+    event.preventDefault();
+    open('mesProjets');
+});
+document.getElementById('competsLink').addEventListener('click', function(event){
+    event.preventDefault();
+    open('ceQueJeSaisFaire');
+});
+
+document.getElementById('close').addEventListener('click', close);
